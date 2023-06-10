e783ab72e82c87a4df4b03fa0bf01d02161020dc
kmtym1998 <kmtym.devloper@gmail.com>
2023-06-10 18:00:39 +0900

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kmtym1998/md-parser
+
+go 1.20
---
+++ parser.go
@@ -0,0 +1,57 @@
+package mdparser
+
+type ParsedMD struct {
+	Raw    string
+	Blocks []BlockContent
+}
+
+type BlockContent struct {
+	Contents []InlineContent
+	Type     BlockContentType
+}
+
+type InlineContent struct {
+	Text        string
+	Type        InlineContentType
+	Src         string
+	Alt         string
+	HasChildren bool
+	Children    []InlineContent
+}
+
+type BlockContentType string
+
+const (
+	BlockContentTypeHeader1     BlockContentType = "header1"
+	BlockContentTypeHeader2     BlockContentType = "header2"
+	BlockContentTypeHeader3     BlockContentType = "header3"
+	BlockContentTypeHeader4     BlockContentType = "header4"
+	BlockContentTypeHeader5     BlockContentType = "header5"
+	BlockContentTypeHeader6     BlockContentType = "header6"
+	BlockContentTypeQuote       BlockContentType = "quote"
+	BlockContentTypeCode        BlockContentType = "code"
+	BlockContentTypeList        BlockContentType = "list"
+	BlockContentTypeOrderedList BlockContentType = "orderedList"
+	BlockContentTypeImage       BlockContentType = "image"
+	BlockContentTypeLink        BlockContentType = "link"
+	BlockContentTypeTable       BlockContentType = "table"
+	BlockContentTypeHorizontal  BlockContentType = "horizontal"
+	BlockContentTypeParagraph   BlockContentType = "paragraph"
+)
+
+type InlineContentType string
+
+const (
+	InlineContentTypeBold      InlineContentType = "bold"
+	InlineContentTypeItalic    InlineContentType = "italic"
+	InlineContentTypeUnderline InlineContentType = "underline"
+	InlineContentTypeStrike    InlineContentType = "strike"
+	InlineContentTypeLink      InlineContentType = "link"
+	InlineContentTypeImage     InlineContentType = "image"
+	InlineContentTypeCode      InlineContentType = "code"
+	InlineContentTypeText      InlineContentType = "text"
+)
+
+func Parse(b []byte) ParsedMD {
+	return ParsedMD{}
+}

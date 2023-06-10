9e2c350c1b499a19536fe2453bf16709708a63b1
kmtym1998 <kmtym.devloper@gmail.com>
2023-06-10 20:11:40 +0900

    ContentType を matcher に移す

--- matcher.go
+++ matcher.go
@@ -2,6 +2,41 @@ package mdparser
 
 import "strings"
 
+type BlockContentType string
+
+const (
+	BlockContentTypeHeader1     BlockContentType = "header1"
+	BlockContentTypeHeader2     BlockContentType = "header2"
+	BlockContentTypeHeader3     BlockContentType = "header3"
+	BlockContentTypeHeader4     BlockContentType = "header4"
+	BlockContentTypeHeader5     BlockContentType = "header5"
+	BlockContentTypeHeader6     BlockContentType = "header6"
+	BlockContentTypeQuote       BlockContentType = "quote"
+	BlockContentTypeCode        BlockContentType = "code"
+	BlockContentTypeList        BlockContentType = "list"
+	BlockContentTypeOrderedList BlockContentType = "orderedList"
+	BlockContentTypeImage       BlockContentType = "image"
+	BlockContentTypeLink        BlockContentType = "link"
+	BlockContentTypeTable       BlockContentType = "table"
+	BlockContentTypeHorizontal  BlockContentType = "horizontal"
+	BlockContentTypeParagraph   BlockContentType = "paragraph"
+	BlockContentTypeEmpty       BlockContentType = "empty"
+	BlockContentTypeUnknown     BlockContentType = "unknown"
+)
+
+type InlineContentType string
+
+const (
+	InlineContentTypeBold      InlineContentType = "bold"
+	InlineContentTypeItalic    InlineContentType = "italic"
+	InlineContentTypeUnderline InlineContentType = "underline"
+	InlineContentTypeStrike    InlineContentType = "strike"
+	InlineContentTypeLink      InlineContentType = "link"
+	InlineContentTypeImage     InlineContentType = "image"
+	InlineContentTypeCode      InlineContentType = "code"
+	InlineContentTypeText      InlineContentType = "text"
+)
+
 type blockContentMatcher interface {
 	match(content string) (BlockContentType, bool)
 }
